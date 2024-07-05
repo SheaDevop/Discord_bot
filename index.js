59adf91a496374fd71c0d5757b39d9563ab96748
Ignacio Dieguez <nacho.dieguez18@gmail.com>
2024-07-05 14:49:30 -0300

    Bot functionality accomplished

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 //require the discord.js classes
 const {Client, Events, GatewayIntentBits} = require('discord.js');
-//create a new client instance
+//create a new client instance aka a bot instance
 const client = new Client({
   intents: [
 		GatewayIntentBits.Guilds,
@@ -11,9 +11,9 @@ const client = new Client({
 		GatewayIntentBits.MessageContent,
 		GatewayIntentBits.GuildMembers
 	]
- });
+});
 
-//when the client is ready run this code only once
+//when the client is ready this code run only once
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
@@ -22,25 +22,32 @@ client.once(Events.ClientReady, readyClient => {
 client.login(process.env.CLIENT_TOKEN);
 
 
-//add role and greet a new user
-// client.on('guildMemberAdd', (guildMember) => {
-//   const channel = client.channels.cache.get('id');
-//   const role = interaction.options.getRole('People');
-//   const member = guildMember.id
-//   if (guildMember.user.bot) return;
-//   else {
-//     channel.send(`Greeting ${guildMember.user.username}`)
-//     member.roles.add(role)
-//   }
-// });
+//greet a new member
+client.on('guildMemberAdd', member => {
+  const channel = client.channels.cache.get('1258503343291105367');
+  if (member.user.bot) return;
+  else {
+    channel.send(`Greetings ${member.user.username}`);
+  }
+});
+
+
+
+//assign a role to new member
+client.on('guildMemberAdd', member => {
+  const role = member.guild.roles.cache.find(role => role.id === '1258506034088120341');
+  member.roles.add(role, 'Welcome to my server');
+});
+
+
 
 //message functionality
 client.on('messageCreate', msg => {
-    if (msg.content === '!help') {
-      msg.reply(`Robots are designed to help you ${msg.author.username}`);
-    } else if (msg.content === '!info') {
-      msg.reply("I'm a Discord bot created by Ignacio Dieguez, your orders are mine.");
-    } else {
-      return;
-    }
+  if (msg.content === '!help') {
+    msg.reply(`Robots are designed to help you ${msg.author.username}`);
+  } else if (msg.content === '!info') {
+    msg.reply("I'm a Discord bot created by Ignacio Dieguez, your orders are mine.");
+  } else {
+    return;
+  }
 });
