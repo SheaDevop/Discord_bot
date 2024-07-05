72626023b9c275cee640158c33b8da9fb5300609
Ignacio Dieguez <nacho.dieguez18@gmail.com>
2024-07-05 12:57:08 -0300

    Message function added

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 //require the discord.js classes
 const {Client, Events, GatewayIntentBits} = require('discord.js');
 //create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers
+	]
+ });
 
 //when the client is ready run this code only once
 client.once(Events.ClientReady, readyClient => {
@@ -14,9 +21,26 @@ client.once(Events.ClientReady, readyClient => {
 //bot log in
 client.login(process.env.CLIENT_TOKEN);
 
-//hello message functionality
-// client.on('messageCreate', msg => {
-//     if (msg.content === 'Hello') {
-//         msg.reply(`Hello ${msg.author.username}`);
-//     }
+
+//add role and greet a new user
+// client.on('guildMemberAdd', (guildMember) => {
+//   const channel = client.channels.cache.get('id');
+//   const role = interaction.options.getRole('People');
+//   const member = guildMember.id
+//   if (guildMember.user.bot) return;
+//   else {
+//     channel.send(`Greeting ${guildMember.user.username}`)
+//     member.roles.add(role)
+//   }
 // });
+
+//message functionality
+client.on('messageCreate', msg => {
+    if (msg.content === '!help') {
+      msg.reply(`Robots are designed to help you ${msg.author.username}`);
+    } else if (msg.content === '!info') {
+      msg.reply("I'm a Discord bot created by Ignacio Dieguez, your orders are mine.");
+    } else {
+      return;
+    }
+});
