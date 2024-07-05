38008d6d896bd883ef0641ddf1cf12f389a93257
Ignacio Dieguez <nacho.dieguez18@gmail.com>
2024-07-05 10:52:05 -0300

    getting the bot online

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 //initialize dotenv
 require('dotenv').config();
 
-//define client intents
-const {Client, GatewayIntentBits} = require('discord.js');
-const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+//require the discord.js classes
+const {Client, Events, GatewayIntentBits} = require('discord.js');
+//create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+//when the client is ready run this code only once
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 //bot log in
 client.login(process.env.CLIENT_TOKEN);
 
 //hello message functionality
-client.on('messageCreate', msg => {
-    if (msg.content === 'Hello') {
-        msg.reply(`Hello ${msg.author.username}`);
-    }
-});
+// client.on('messageCreate', msg => {
+//     if (msg.content === 'Hello') {
+//         msg.reply(`Hello ${msg.author.username}`);
+//     }
+// });
